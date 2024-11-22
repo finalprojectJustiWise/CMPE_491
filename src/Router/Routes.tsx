b3dcbc74eeaf60ac77bb1c5410edb084cc29d58c
Cicikuss <ahmetbyk49@gmail.com>
2024-11-22 21:45:15 +0300

    fixed2

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '../Pages/HomePage/HomePage'
 import TeamPage from '../Pages/TeamPage/TeamPage'
 
-type Props = {}
 
 export const router = createBrowserRouter([
     {path: "/",element:<HomePage/>},

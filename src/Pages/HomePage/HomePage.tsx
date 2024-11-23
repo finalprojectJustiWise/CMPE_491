1aff0ef2f9ff572e6702dee546d156cf1cd0a5b0
Aslil <aslilgn@gmail.com>
2024-11-23 03:40:05 +0300

    reports added

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -8,37 +8,50 @@ const HomePage = () => {
     <>
       <Navbar />
       <div className="image-container">
-        <img id='justiceimg' src={justice} alt="Adalet mülkün temelidir" />
+        <img id="justiceimg" src={justice} alt="Adalet mülkün temelidir" />
         <div className="overlay-text">
           <h1>Project Overview</h1>
           <div>Metin Girin</div>
+
           <h1>Project Objective</h1>
           <div>Cicikuş</div>
+
           <h1>Features</h1>
           <ul>
             <li>Merhaba nasılsın burnun kapıya kısılsın</li>
           </ul>
+
           <h1>Technologies</h1>
           <ul>
             <li>Merhaba nasılsın burnun kapıya kısılsın</li>
           </ul>
+
           <h1>Conclusion</h1>
           <div>
-            {/* Yeni Kod: İndirme Butonları */}
+            {/* Dosya indirme butonları */}
             <h1>Project Downloads</h1>
             <ul>
               <li>
-              <a href="/ProjectProposal.zip" download="ProjectProposal">
-              <button>Download Project Proposal</button>
+                <a
+                  href="https://finalprojectjustiwise.github.io/CMPE_491/ProjectProposal.zip"
+                  download="ProjectProposal"
+                >
+                  <button>Download Project Proposal</button>
                 </a>
               </li>
               <li>
-              <a href="/ProjectSpecificationsReport.zip" download="ProjectSpecificationsReport">
-              <button>Download Project Specifications Report</button>
+                <a
+                  href="https://finalprojectjustiwise.github.io/CMPE_491/ProjectSpecificationsReport.zip"
+                  download="ProjectSpecificationsReport"
+                >
+                  <button>Download Project Specifications Report</button>
                 </a>
               </li>
               <li>
-                <a href="/ProjectAnalysisReport.zip" download="ProjectAnalysisReport">
+                <a
+                  href="https://finalprojectjustiwise.github.io/CMPE_491/ProjectAnalysisReport.zip"
+                  download="ProjectAnalysisReport"
+                >
                   <button>Download Project Analysis Report</button>
                 </a>
               </li>

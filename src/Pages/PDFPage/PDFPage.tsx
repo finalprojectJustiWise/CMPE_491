b35dfeb43bc95c08ed648abc4da0f80dfbc78c5f
Cicikuss <ahmetbyk49@gmail.com>
2024-11-26 02:03:17 +0300

    update pdfpage

--- src/Pages/PDFPage/PDFPage.tsx
+++ src/Pages/PDFPage/PDFPage.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
-import file from '../../Assets/ProjectSpecificationsReport.pdf';
 import './PDFPage.css';
 import Navbar from '../../Components/Navbar/Navbar';
 
 
+const pdfFiles = [
+  { name: "Project Specifications", path: require('../../Assets/ProjectSpecificationsReport.pdf') },
+  { name: "Project Analysis", path: require('../../Assets/ProjectAnalysisReport.pdf') },
+  { name: "Project Proposal", path: require('../../Assets/ProjectProposal.pdf') },
+];
 
 const PDFPage = () => {
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [selectedFile, setSelectedFile] = useState<string | ArrayBuffer | null>(pdfFiles[0].path); 
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
@@ -22,12 +27,32 @@ const PDFPage = () => {
     if (numPages && pageNumber < numPages) setPageNumber(pageNumber + 1);
   };
 
+  const handleFileSelection = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedPath = event.target.value;
+    setSelectedFile(selectedPath);  
+    setPageNumber(1);  
+  };
+
   return (
     <div className="pdf-container">
-      <Navbar/>
-      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} renderTextLayer={false} renderAnnotationLayer={false}/>
-      </Document>
+      <Navbar />
+      <div className="file-picker-container">
+        <h3>Select a PDF File:</h3>
+        <select onChange={handleFileSelection} className="file-picker">
+          {pdfFiles.map((file, index) => (
+            <option key={index} value={file.path}>
+              {file.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {selectedFile && (
+        <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} renderTextLayer={false} renderAnnotationLayer={false} />
+        </Document>
+      )}
+
       <div className="pagination-controls">
         <button onClick={goToPrevPage} disabled={pageNumber === 1}>
           Previous
